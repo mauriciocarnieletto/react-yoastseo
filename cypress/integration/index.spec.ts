86db9a15274033d7cccf8e8c71f39df084288660
lmarqs <lucas.paula@wipro.com>
2020-12-15 16:28:16 -0300

    chore: adjust tests

--- cypress/integration/index.spec.ts
+++
@@ -1,6 +0,0 @@
-it("should access a global var called ReactYoastSeo", () => {
-  cy.visit("http://localhost:5000");
-  cy.window().then(({ ReactYoastSeo }) => {
-    expect(ReactYoastSeo === undefined).not.to.be.true;
-  });
-});
--- cypress/support/react-yoastseo.d.ts
+++
@@ -1,9 +0,0 @@
-import * as ReactYoastSeo from "@/index";
-
-export {};
-
-declare global {
-  interface Window {
-    readonly ReactYoastSeo: typeof ReactYoastSeo;
-  }
-}
